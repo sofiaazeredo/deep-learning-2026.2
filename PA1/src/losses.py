27bef8ffa9f2a5d7ee22435345d3350d670071c5
sofia.azeredo <sofia.moumont@gmail.com>
2026-09-12 04:28:47 -0300

    boundary entropy loss

--- PA1/src/losses.py
+++ PA1/src/losses.py
@@ -24,3 +24,25 @@ class CrossEntropyLoss(nn.Module):
         target = semantic_mask(instance_mask)
 
         return self.loss(prediction, target)
+
+
+class BoundaryCrossEntropyLoss(nn.Module):
+
+    def __init__(self, class_weights=None):
+        super().__init__()
+
+        if class_weights is not None:
+            class_weights = torch.tensor(
+                class_weights,
+                dtype=torch.float32
+            )
+
+        self.loss = nn.CrossEntropyLoss(
+            weight=class_weights
+        )
+
+    def forward(self, prediction, boundary_target):
+        return self.loss(
+            prediction,
+            boundary_target
+        )
